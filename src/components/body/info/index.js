988b32c233da6c5bed19bd62efdeb7b20537a68a
Azk <azkriven17@gmail.com>
2022-02-07 10:54:55 +0800

    responsive info section

--- src/components/body/info/index.js
+++ src/components/body/info/index.js
@@ -1,36 +1,32 @@
 import React from "react";
-import styled from "styled-components";
-import Seperator from "../../common/seperator";
+import "./info.css";
 import image from "../../../assets/coding.gif";
-
-const index = () => {
+import SocialContact from "../../common/social-contact";
+const About = () => {
   return (
-    <>
-      <Seperator />
-      <div className='section-title'>About Me</div>
-      <AboutMe>
-        <p>
-          Since I was a kid I was always interested and curious about computers.
-          I am constantly learning more about web development and some of the
-          technologies I'm currently learning are <strong>NextJS</strong>,
-          <strong>GraphQL</strong> and I also want to learn more about BackEnd
-          Development
-        </p>
+    <div className='about'>
+      <div className='about-top'>
         <div className='about-photo'>
           <img src={image} className='picture' />
         </div>
-      </AboutMe>
-    </>
+        <div className='about-info'>
+          <div class='containerr'>
+            <p className='my-details'>
+              Since I was a kid I was always interested and curious about
+              computers. I am constantly learning more about web development and
+              I'm aiming to be a full stack dev.
+            </p>
+            <p className='my-details'>
+              The technologies I'm currently learning are
+              <strong> NextJS, material-ui</strong> and some concepts like
+              authentication and state management systems.
+            </p>
+          </div>
+        </div>
+      </div>
+      <SocialContact />
+    </div>
   );
 };
-const AboutMe = styled.div`
-  font-size: 1.5rem;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  .section-title {
-    font-size: 32px;
-    font-weight: 700;
-  }
-`;
-export default index;
+
+export default About;
